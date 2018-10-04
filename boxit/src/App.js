447f08d08cc2ffe707006a3acd2212f22c7ceb1b
Stonedude <mart-wes@hotmail.com>
2018-10-04 11:01:38 +0200

    Added basic styling with CSS grids, going for niceness after

--- boxit/src/App.js
+++ boxit/src/App.js
@@ -6,11 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
+        <div className="grid-item" id="logo"> Logoting</div>
+        <div className="grid-item" id="title">Titletuff</div>
+        <button className="grid-item" id="button">Buttonhere</button>
+
+        <div className="grid-item" id="content1">Dis a car</div>
+        <div className="grid-item" id="content2">Dis a snake</div>
+        <div className="grid-item" id="content3">Turtles here</div>
+        <div className="grid-item" id="sidebar1">Shoppo stuff</div>
+        <div className="grid-item" id="sidebar2">More things here</div>
+        
+        <div className="grid-item" id="bottom">This is the end</div>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,7 +26,6 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
       </div>
     );
   }

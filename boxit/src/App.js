c8a4c0ffb533a08297fbb12e2ab8672063307cb3
Thommath <thomlmath@gmail.com>
2018-10-04 12:12:29 +0200

    Added pages

--- boxit/src/App.js
+++ boxit/src/App.js
@@ -7,7 +7,15 @@ class App extends Component {
 
   constructor() {
     super();
-      this.state = {token: '', accounts: []}
+      this.state = {
+        token: '',
+        accounts: [],
+        page: 0,
+      }
+  }
+
+  setPage = (n) => {
+    this.setState({page: n})
   }
 
   login = () => {
@@ -46,25 +54,53 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {this.state.page == 0 &&
+          <div>
+            <div className="grid-item" id="logo"> Logoting</div>
+            <div className="grid-item" id="title">Titletuff</div>
+            <button className="grid-item" id="button" onClick={() => this.setPage(1)}>Pay monies</button>
 
-        <div className="grid-item" id="logo"> Logoting</div>
-        <div className="grid-item" id="title">Titletuff</div>
-        <button className="grid-item" id="button" onClick={this.login}>Pay monies</button>
+            <div className="grid-item" id="content1">Dis a car</div>
+            <div className="grid-item" id="content2">Dis a snake</div>
+            <div className="grid-item" id="content3">Turtles here</div>
+            <div className="grid-item" id="sidebar1">Shoppo stuff</div>
+            <div className="grid-item" id="sidebar2">More things here</div>
 
-        <div className="grid-item" id="content1">Dis a car</div>
-        <div className="grid-item" id="content2">Dis a snake</div>
-        <div className="grid-item" id="content3">Turtles here</div>
-        <div className="grid-item" id="sidebar1">Shoppo stuff</div>
-        <div className="grid-item" id="sidebar2">More things here</div>
+            <div className="grid-item" id="bottom">This is the end</div>
 
-        <div className="grid-item" id="bottom">This is the end</div>
+            <a onClick={this.getAccounts}>Login to sparebanken</a>
+            <a onClick={this.makeTransaction}>Make transaction</a>
+            <a onClick={this.confirmTransaction}>Confirm</a>
+            {this.state.token && <p>{this.state.token}</p>}
+            {this.state.accounts.length > 0 && <p>{this.state.accounts[0].accountNumber.value}</p>}
+            {this.state.transaction && <div><p>Amount: {this.state.transaction.amount}</p><p>Status: {this.state.transaction.signingStatus}</p></div>}
+          </div>
+        }
 
-        <a onClick={this.getAccounts}>Login to sparebanken</a>
-        <a onClick={this.makeTransaction}>Make transaction</a>
-        <a onClick={this.confirmTransaction}>Confirm</a>
-        {this.state.token && <p>{this.state.token}</p>}
-        {this.state.accounts.length > 0 && <p>{this.state.accounts[0].accountNumber.value}</p>}
-        {this.state.transaction && <div><p>Amount: {this.state.transaction.amount}</p><p>Status: {this.state.transaction.signingStatus}</p></div>}
+        {this.state.page == 1 &&
+          <div>
+            <div className="grid-item" id="logo"> BoxIt</div>
+            <div className="grid-item" id="title">BOxibot</div>
+            <button className="grid-item" id="button" onClick={() => this.setPage(2)}>Pay monies</button>
+
+            <div className="grid-item" id="content1">Dis a car</div>
+            <div className="grid-item" id="content2">Dis a snake</div>
+            <div className="grid-item" id="content3">Turtles here</div>
+            <div className="grid-item" id="sidebar1">Shoppo stuff</div>
+            <div className="grid-item" id="sidebar2">More things here</div>
+
+            <div className="grid-item" id="bottom">This is the end</div>
+            
+            <a onClick={this.login}>Login</a>
+            <a onClick={this.getAccounts}>Login to sparebanken</a>
+            <a onClick={this.makeTransaction}>Make transaction</a>
+            <a onClick={this.confirmTransaction}>Confirm</a>
+            {this.state.token && <p>{this.state.token}</p>}
+            {this.state.accounts.length > 0 && <p>{this.state.accounts[0].accountNumber.value}</p>}
+            {this.state.transaction && <div><p>Amount: {this.state.transaction.amount}</p><p>Status: {this.state.transaction.signingStatus}</p></div>}
+          </div>
+        }
+        
       </div>
     );
   }
